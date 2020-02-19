e0f7a55c3a4b2e017b448bacdd44e948c48cca81
aaronium112 <aaron@sphericalwave.com>
2020-02-19 11:17:31 -0400

    initial commit

---
+++ Sources/BasicAsync/Database.swift
@@ -0,0 +1,94 @@
+
+
+import NIO
+import Foundation
+
+/// Entities that can be stored in the mock database provided below
+public protocol DatabaseEntity
+{
+  associatedtype Identifier: Equatable
+  var id: Identifier { get }   /// Used for `find` and `delete` operations
+}
+
+/// A mock database generic to a single DatabaseEntity type
+///
+/// All results are returned by completing the provided promise
+/// This ensures that the results are passed to the EventLoop the EventLoopPromise originated from
+public final class Database<Entity: DatabaseEntity>
+{
+  /// The database has it's own eventloop because it's shared between many threads
+  ///
+  /// To ensure all entity access is thread safe, all operations happen on this EventLoop
+  private let eventLoop: EventLoop
+
+  private var entities = [Entity]()   /// The internal storage of the mock database
+
+  /// Lists all entities in the database and returns them by completing the promise with this result
+  public func getAllEntities(completing promise: EventLoopPromise<[Entity]>) {
+    eventLoop.execute {
+      promise.succeed(result: self.entities)
+    }
+  }
+
+  /// Deletes a single entity
+  ///
+  /// Succeeds when the deletion was successful and fails if no entity was found to remove
+  public func deleteOne(by id: Entity.Identifier, completing promise: EventLoopPromise<Void>) {
+    eventLoop.execute {
+      for i in 0..<self.entities.count {
+        if self.entities[i].id == id {
+          self.entities.remove(at: i)
+          promise.succeed(result: ())
+          return
+        }
+      }
+
+      promise.fail(error: DatabaseError.entityNotFound)
+    }
+  }
+
+  /// Finds a single entity by it's identifier and completed the promise with the result
+  ///
+  /// The promise will be completed with `nil` if the entity was not found
+  public func findOne(by id: Entity.Identifier, completing promise: EventLoopPromise<Entity?>) {
+    eventLoop.execute {
+      for entity in self.entities where entity.id == id {
+        promise.succeed(result: entity)
+        return
+      }
+
+      promise.succeed(result: nil)
+    }
+  }
+
+  /// Adds an entity to the database
+  ///
+  /// This operation can fail if an entity with this ID already exists
+  public func addEntity(_ newEntity: Entity, completing promise: EventLoopPromise<Void>) {
+    eventLoop.execute {
+      for entity in self.entities {
+        if entity.id == newEntity.id {
+          promise.fail(error: DatabaseError.entityAlreadyExists)
+          return
+        }
+      }
+
+      self.entities.append(newEntity)
+      promise.succeed(result: ())
+    }
+  }
+
+  /// Creates a new mock database for storing the generic `Entity`
+  public init() {
+    eventLoop = MultiThreadedEventLoopGroup(numberOfThreads: 1).next()
+  }
+}
+
+/// The databsae can fail with these status codes
+///
+/// Most databases will have too many errors to sum up simply in an enum
+/// and are implemented outside of the application's access
+fileprivate enum DatabaseError: Error {
+  case entityAlreadyExists
+  case entityNotFound
+}
---
+++ Sources/BasicAsync/main.swift
@@ -0,0 +1,34 @@
+
+import NIO
+import NIOHTTP1
+
+let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+
+let bootstrap = ServerBootstrap(group: group)
+  // Specifies that we can have 256 TCP sockets waiting to be accepted for processing at a given time
+  .serverChannelOption(ChannelOptions.backlog, value: 256)
+  // Allows reusing the IP address and port so that multiple threads can receive clients
+  .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+  
+  // Sets up the HTTP parser and handler on the SwiftNIO channel
+  .childChannelInitializer { channel in
+    channel.pipeline.configureHTTPServerPipeline(withErrorHandling: true).then {
+      channel.pipeline.add(handler: HTTPHandler(responder: QuoteResponder()))
+    }
+}
+
+// The host and port to which the webserver will bind
+// ::1 can usually be accessed as `localhost`
+// The difference is that `::1` is also available from other computers
+let host = "::1"
+let port = 8080
+
+let serverChannel = try bootstrap.bind(host: host, port: port).wait()
+
+guard serverChannel.localAddress != nil else {
+  fatalError("Unable to bind to \(host) at port \(port)")
+}
+
+print("Server started and listening")
+
+try serverChannel.closeFuture.wait()
